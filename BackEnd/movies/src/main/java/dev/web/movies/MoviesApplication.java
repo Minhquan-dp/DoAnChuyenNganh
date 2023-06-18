2cec5b8fd056d233e3e7c4a60f69a8a0e3f58cdb
Minhquan-dp <19130285@st.hcmuaf.edu.vn>
2023-06-18 21:17:48 +0700

    first commit

---
+++ BackEnd/movies/src/main/java/dev/web/movies/MoviesApplication.java
@@ -0,0 +1,13 @@
+package dev.web.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+}
