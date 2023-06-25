dcbe1e2e7be42a005d64e87b1035e6114236d28e
Le Quang Phuoc <19130295@st.hcmuaf.edu.vn>
2023-06-25 23:50:30 +0700

    Add BillService

---
+++ BackEnd/movies/src/main/java/dev/web/movies/apis/BillApi.java
@@ -0,0 +1,26 @@
+package dev.web.movies.apis;
+
+import dev.web.movies.dtos.BookingRequestDTO;
+import dev.web.movies.services.IBillService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/bills")
+public class BillApi {
+    @Autowired
+    private IBillService billService;
+
+    @PostMapping("/create-new-bill")
+    public ResponseEntity<String> createNewBill(@RequestBody BookingRequestDTO bookingRequestDTO) {
+        try {
+            billService.createNewBill(bookingRequestDTO);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.EXPECTATION_FAILED);
+        }
+        return new ResponseEntity<>("Bạn đã mua vé xem phim thành công !", HttpStatus.OK);
+    }
+}
